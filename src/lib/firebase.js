1e91a7a95d1b490c7197fa0bf6a13c374b9531bb
vitoria-junker <vickjunker14@gmail.com>
2023-05-22 15:16:37 -0300

    alterando as variáveis de ambiente

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -3,12 +3,12 @@ import { getStorage, ref } from 'firebase/storage';
 import { initializeApp } from 'firebase/app';
 
 const firebaseConfig =    { 
-apiKey: process.env.FIREBASE_API_KEY,
-authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-projectId: process.env.FIREBASE_PROJECT_ID,
+apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
 storageBucket: 'gs://portf0lio-2b546.appspot.com',
-messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-appId: process.env.FIREBASE_API_ID,
+messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+appId: process.env.NEXT_PUBLIC_FIREBASE_API_ID,
 }
 
 const app = initializeApp(firebaseConfig);
--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -5,13 +5,13 @@ import GoogleProvider from 'next-auth/providers/google'
 export default NextAuth({
     providers: [
         GithubProvider({
-            clientId:process.env.GITHUB_ID,
-            clientSecret:process.env.GITHUB_SECRET
+            clientId:process.env.NEXT_PUBLIC_GITHUB_ID,
+            clientSecret:process.env.NEXT_PUBLIC_GITHUB_SECRET
         }),
         GoogleProvider({
-            clientId: process.env.GOOGLE_ID,
-            clientSecret: process.env.GOOGLE_SECRET
+            clientId: process.env.NEXT_PUBLIC_GOOGLE_ID,
+            clientSecret: process.env.NEXT_PUBLIC_GOOGLE_SECRET
         }),
     ], 
-    secret: process.env.SECRET  
+    secret: process.env.NEXT_PUBLIC_SECRET  
 }) 

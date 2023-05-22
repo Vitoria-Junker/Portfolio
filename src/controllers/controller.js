30683050a3a6b220871de1ee8107a5bdd0b968bc
vitoria-junker <vickjunker14@gmail.com>
2023-05-22 16:59:34 -0300

    correção de uma variavel

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -1,4 +1,4 @@
-import postProject from "@/api-routes/routes";
+import { postProject } from "@/api-routes/routes";
 
 export const createNewProject = async (formValues, imgURL) => {
   await postProject({ ...formValues, img: imgURL });

66d5f9a57ac73bd4312234503cc78e3646844590
Vitoria-Junker <vickjunker14@gmail.com>
2024-02-21 23:02:14 -0300

    update footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,8 +1,8 @@
 export default function Footer () {
     return (
-        <footer className="bg-none py-4">
+        <footer className="bg-none pt-4">
       <div className="container mx-auto text-center">
-      <p className="text-xs text-gray-200 font-sans">&copy; {new Date().getFullYear()} Vitória Junker. Todos os direitos reservados.</p>
+      <p className="text-xs text-slate-400 font-sans" style={{ textShadow: "1px 1px 1px rgba(0,0,0,0.6)" }}>&copy; {new Date().getFullYear()} Vitória Junker. Todos os direitos reservados.</p>
       </div>
     </footer>
     )
